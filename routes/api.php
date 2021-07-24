597b292e100c9d5fdf164103c2bfe7ea70ccfa92
Tobi Said <tobisaid007@gmail.com>
2021-07-24 12:03:04 +0100

    Authentication

--- routes/api.php
+++ routes/api.php
@@ -28,3 +28,12 @@ Route::group(['prefix' => 'posts'],function(){
     Route::get('',[PostController::class,'index']);
     Route::post('/comment',[PostController::class,'addComment']);
 });
+
+// Route::group(['middleware' => 'api','prefix' => 'auth'], function ($router) {
+
+//     Route::post('login', 'AuthController@login');
+//     Route::post('logout', 'AuthController@logout');
+//     Route::post('refresh', 'AuthController@refresh');
+//     Route::post('me', 'AuthController@me');
+
+// });
